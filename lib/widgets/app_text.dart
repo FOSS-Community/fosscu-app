fbbfd3ce3009a8f1a50308252a59faccebb75bb2
Kanishk Pachauri <krishna03012003@icloud.com>
2022-10-08 03:12:24 +0530

    Code of Text assets 𑗉

---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SmallText extends StatefulWidget {
+  const SmallText({super.key});
+
+  @override
+  State<SmallText> createState() => _SmallTextState();
+}
+
+class _SmallTextState extends State<SmallText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
