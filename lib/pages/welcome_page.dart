379f9958a6a04dc6b96ec4e56ec104e375e03964
Kanishk Pachauri <krishna03012003@icloud.com>
2022-10-08 03:38:06 +0530

    fixed unused imports.

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatefulWidget {
