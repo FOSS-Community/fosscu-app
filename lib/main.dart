e7fe7f92ea5c1f628a6636dc709255eb0fba3375
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-02 18:17:34 +0530

    hide the nav bar on android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fosscu_app/screens/homepage.dart';
 
 void main() {
@@ -6,9 +7,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+      // Hide the Android navigation bar
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage()
