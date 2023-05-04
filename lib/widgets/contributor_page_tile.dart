6451658d74f6f253203a8e94ad2510b27aed6d55
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-04 23:49:07 +0530

    added gesture detector in Contributor page tile

--- lib/widgets/contributor_page_tile.dart
+++ lib/widgets/contributor_page_tile.dart
@@ -18,38 +18,56 @@ class ContributionTile extends StatefulWidget {
 }
 
 class _ContributionTileState extends State<ContributionTile> {
+  bool _isTappedDown = false;
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    return Container(
-      width: screenWidth * 0.32,
-      height: screenWidth * 0.32,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25), color: midGreyColor),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(bottom: 3),
-            width: screenWidth * 0.15,
-            height: screenWidth * 0.15,
-            child: SvgPicture.asset(widget.picture),
-          ),
-           SizedBox(height: screenHeight * 0.005,),
-          Container(
-            alignment: Alignment.center,
-            width: screenWidth * 0.22,
-            child: Text(
-              widget.text,
-              
-              style: GoogleFonts.chivo(
-                height: 1.5,
-                  color: Colors.white, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTapDown: (details) {
+        setState(() {
+          _isTappedDown = true;
+        });
+      },
+      onTapUp: (details) {
+        setState(() {
+          _isTappedDown = false;
+        });
+      },
+      
+           child: Container(
+        width: screenWidth * 0.32,
+        height: screenWidth * 0.32,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: _isTappedDown? blackColor: midGreyColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 3),
+              width: screenWidth * 0.15,
+              height: screenWidth * 0.15,
+              child: SvgPicture.asset(widget.picture),
             ),
-          )
-        ],
+            SizedBox(
+              height: screenHeight * 0.005,
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: screenWidth * 0.22,
+              child: Text(
+                widget.text,
+                style: GoogleFonts.chivo(
+                    height: 1.5,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
