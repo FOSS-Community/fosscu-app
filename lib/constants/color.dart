1cfb9f911997b1278d56981bd0217076cd290334
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-04 22:00:45 +0530

    added more colors as const

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -5,5 +5,6 @@ const Color greenTrackColor = Color.fromARGB(62, 0, 255, 17);
 const Color blackColor = Colors.black;
 const Color blueGreenColor = Color.fromARGB(255, 0, 233, 156);
 const Color darkGreyColor = Color.fromARGB(255, 27, 27, 27);
+const Color midGreyColor = Color.fromARGB(255, 31, 31, 31);
 const Color brightGreyColor = Color.fromARGB(255, 70, 70, 70);
 const Color tileColor = Color.fromARGB(255, 46, 52, 68);
