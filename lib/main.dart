99a38306ae1d0d6aa21fa0fbb26c7089795c5ad6
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-17 16:30:35 +0530

    created a page to check if the user is logged in? and navigate to correct page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fosscu_app/screens/auth_page_screens/auth_page.dart';
 import 'package:fosscu_app/widgets/navbar.dart';
 
 void main() {
@@ -14,6 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(useMaterial3: true),
-        home: const NavBarScreen());
+        home: const AuthPage());
   }
 }
---
+++ lib/screens/auth_page_screens/auth_page.dart
@@ -0,0 +1,30 @@
+/// called by main function
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:fosscu_app/widgets/navbar.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+
+        /// THIS IS CONSTANTLY GOING TO CHECK IF THE USER IS SIGNED IN OR NOT
+        builder: (context, snapshot) {
+          /// IF the user is logged in
+          if(snapshot.hasData){
+            return const NavBarScreen();
+          }
+          else {
+            return const LoginOrRegisterPage();
+          }
+        },
+      ),
+    );
+  }
+}
