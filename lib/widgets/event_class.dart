d7af76a87faabd2fe28949a80bcec0a0eb0437a3
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-07-08 18:58:08 +0530

    added eventId in the eventClass

--- lib/widgets/event_class.dart
+++ lib/widgets/event_class.dart
@@ -3,11 +3,13 @@ class Event {
   final String eventDates;
   final String eventLumaLink;
   final String eventHost;
+  final String eventID;
 
   Event({
     required this.eventThumbnail,
     required this.eventDates,
     required this.eventLumaLink,
     required this.eventHost,
+    required this.eventID,
   });
 }
