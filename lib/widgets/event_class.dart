a3b5dac6260012abdb62ae938f4bdb27d3d7c163
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-07-08 18:27:01 +0530

    created event class

---
+++ lib/widgets/event_class.dart
@@ -0,0 +1,13 @@
+class Event {
+  final String eventThumbnail;
+  final String eventDates;
+  final String eventLumaLink;
+  final String eventHost;
+
+  Event({
+    required this.eventThumbnail,
+    required this.eventDates,
+    required this.eventLumaLink,
+    required this.eventHost,
+  });
+}
