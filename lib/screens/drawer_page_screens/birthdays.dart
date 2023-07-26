6c3716eecfaecf91d02cbf902b65f541db10f480
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-07-26 11:14:59 +0530

    added progress indicator in birthday page

--- lib/screens/drawer_page_screens/birthdays.dart
+++ lib/screens/drawer_page_screens/birthdays.dart
@@ -14,6 +14,7 @@ class BirthdayScreen extends StatefulWidget {
 }
 
 class _BirthdayScreenState extends State<BirthdayScreen> {
+  bool _isLoading = true;
   String dob = 'your dob';
   // stream method to get birthdays from airtable database
   Stream<List<dynamic>> getBirthdays() async* {
@@ -31,6 +32,9 @@ class _BirthdayScreenState extends State<BirthdayScreen> {
     }).toList();
 
     yield birthdayList;
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -40,41 +44,67 @@ class _BirthdayScreenState extends State<BirthdayScreen> {
     return Scaffold(
       backgroundColor: blackColor,
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            SizedBox(height: screenWidth * 0.05),
-            Container(
-                margin: EdgeInsets.all(screenWidth * 0.04),
-                child: Text(
-                  'Birthdays of FOSS Community members',
-                  style: GoogleFonts.leagueSpartan(
-                      color: Colors.white, fontSize: screenWidth * 0.05),
-                )),
-            Container(
-              margin: EdgeInsets.all(screenWidth * 0.05) ,
-              child: StreamBuilder(
-                  stream: birthdayStream,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final birthdays = snapshot.data!;
-                      return SizedBox(
-                        height: screenWidth * 1,
-                        child: ListView.builder(
-                            itemCount: birthdays.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(
-                                  birthdays[index]['Name'] ?? '',
-                                  style: const TextStyle(color: Colors.white),
-                                ),
-                                subtitle: Text(birthdays[index]['dob'] ?? '', style: const TextStyle(color: Colors.white),),
-                              );
-                            }),
-                      );
-                    } else if (snapshot.hasError) {}
-                    return Container();
-                  }),
-            )
+            Column(
+              children: [
+                SizedBox(height: screenWidth * 0.05),
+                Container(
+                    margin: EdgeInsets.all(screenWidth * 0.04),
+                    child: Text(
+                      'Birthdays of FOSS Community members',
+                      style: GoogleFonts.leagueSpartan(
+                          color: Colors.white, fontSize: screenWidth * 0.05),
+                    )),
+                Container(
+                  margin: EdgeInsets.all(screenWidth * 0.05),
+                  child: StreamBuilder(
+                      stream: birthdayStream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          final birthdays = snapshot.data!;
+                          return SizedBox(
+                            height: screenWidth * 1,
+                            child: ListView.builder(
+                                itemCount: birthdays.length,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    title: Text(
+                                      birthdays[index]['Name'] ?? '',
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                    subtitle: Text(
+                                      birthdays[index]['dob'] ?? '',
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                  );
+                                }),
+                          );
+                        } else if (snapshot.hasError) {}
+                        return Container();
+                      }),
+                )
+              ],
+            ),
+            Visibility(
+              visible: _isLoading,
+              child: Center(
+                child: Container(
+                  width: screenWidth * 0.7,
+                  height: screenWidth * 0.3,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white),
+                  child: const Center(
+                    child: CircularProgressIndicator(
+                      color: blackColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
